ab9b9664696087c64ce83c07a8ca5e02d1c98215
GuptaShubham-11 <shu919847@gmail.com>
2024-12-02 18:45:22 +0530

    add store & features using RTK

---
+++ src/features/canvasSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    tool: 'pen', // Active tool
+    color: '#000000', // Current color
+    strokeWidth: 2, // Brush size
+    eraserWidth: 2, // Eraser size
+}
+
+const canvasSlice = createSlice({
+    name: 'canvas',
+    initialState,
+    reducers: {
+        setTool: (state, action) => {
+            state.tool = action.payload;
+        },
+        setColor: (state, action) => {
+            state.color = action.payload;
+        },
+        setStrokeWidth: (state, action) => {
+            state.strokeWidth = action.payload;
+        },
+        setEraserWidth: (state, action) => {
+            state.eraserWidth =  action.payload;
+        }
+    }
+});
+
+export const {setTool, setColor, setStrokeWidth, setEraserWidth} = canvasSlice.actions
+export default canvasSlice.reducer
